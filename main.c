9ac0deb1d4f12e5019b0dbab9b596c7b833e7529
Razmik Ovnanian <rovnania@c1r3s3.42yerevan.am>
2026-03-05 16:13:41 +0400

    alpha parser

--- main.c
+++ main.c
@@ -6,34 +6,77 @@
 /*   By: rovnania <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 19:13:22 by rovnania          #+#    #+#             */
-/*   Updated: 2026/03/02 19:13:17 by rovnania         ###   ########.fr       */
+/*   Updated: 2026/03/05 16:10:42 by rovnania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+int	ft_parser(char const *argv[], int);
+int	num_check(char const *str);
 
 int main(int argc, char const *argv[])
 {
 	if (argc == 1)
 		return (0);
-	if (flag_check(*argv))
-	{
-		return (10);
-	}
+	printf("count of nuumbers: %d\n", ft_parser(argv, argc));
 	return (0);
 }
 
+int	ft_parser(char const *argv[], int argc)
+{
+	int	i;
+	int	j;
+	int k;
+	
+	k = 0;
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		if (ft_isdigit(argv[i][j]) || (argv[i][j] == '-' && ft_isdigit(argv[i][j + 1])))
+		{
+			k++;
+			j++;
+		}
+		
+		if (!num_check(argv[i]))
+			return (0);
+		i++;
+	}
+	return (k);
+}
+
+int	num_check(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
 int	flag_check(char const *argv)
 {
 	if (ft_memcmp(argv, "--adaptive", 11))
 		return (1);
-	if (ft_memcmp(*argv, "--simple", 9))
+	else if (ft_memcmp(argv, "--simple", 9))
 		return (2);
-	if (ft_memcmp(*argv, "--medium", 9))
+	else if (ft_memcmp(argv, "--medium", 9))
 		return (3);
-	if (ft_memcmp(*argv, "--complex", 10))
+	else if (ft_memcmp(argv, "--complex", 10))
 		return (4);
-	if (ft_memcmp(*argv, "--brench", 9))
-		return (5);
+	else
+		return (0);
+}
+
+int	bench_check(char const *argv)
+{
+	if (ft_memcmp(argv, "--bench", 8))
+		return (1);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,17 +6,18 @@
 /*   By: rovnania <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 19:14:00 by rovnania          #+#    #+#             */
-/*   Updated: 2026/03/02 17:21:11 by rovnania         ###   ########.fr       */
+/*   Updated: 2026/03/05 15:44:53 by rovnania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft/libft.h"
+#include <stdio.h>
 // # include <unistd.h>
 // # include <stdlib.h>
 // # include "ft_printf.h"
 
-int	flag_check(char const **argv);
+int	flag_check(char const *argv);
 
 #endif
